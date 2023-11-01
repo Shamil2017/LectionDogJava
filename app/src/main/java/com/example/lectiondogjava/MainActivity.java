813c08e58f89e8508fac97a809e3971c57439105
OtsokovShA <shamil24@mail.ru>
2023-11-01 09:29:06 +0300

    Создание функции загрузки JSON

--- app/src/main/java/com/example/lectiondogjava/MainActivity.java
+++ app/src/main/java/com/example/lectiondogjava/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.lectiondogjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String Base_Url = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadImage();
+    }
+
+    private void loadImage() {
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            url = new URL(Base_Url);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String res = reader.readLine();
+            Log.d("MainActivity",res);
+
+
+        } catch (Exception e) {
+            Log.d("MainActivity", e.toString());
+
+        }
     }
 }
